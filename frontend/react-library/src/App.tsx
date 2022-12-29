8cc7cc5c1add340f838fb3b3b24088f6a9e519a3
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-29 14:18:28 -0800

    navbar setup. closes #4

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>welcome</h1>
-    </div>
+    <Navbar/>
   );
 }
 
