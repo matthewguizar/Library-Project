b2501bb90bddc1f299b62f272fbd839c9043ea09
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-29 17:04:43 -0800

    added footer and library services. closes #8 and #9

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -4,6 +4,8 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/Homepage/ExploreTopBooks';
 import { Carousel } from './layouts/Homepage/Carousel';
 import { Heros } from './layouts/Homepage/Heros';
+import { LibraryServices } from './layouts/Homepage/LibraryServices';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
       <ExploreTopBooks/>
       <Carousel/>
       <Heros/>
-
+      <LibraryServices/>
+      <Footer/>
     </div>
   );
 }
