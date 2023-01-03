4bd0dbe919f8c788e371ddc4bf52170bfef78071
Matthew Guizar <matthewrguizar@gmail.com>
2023-01-03 13:54:54 -0800

    added search books page component

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Homepage } from './layouts/Homepage/HomePage';
+import  {SearchBooksPage} from './layouts/SearchBooksPage/SearchBooksPage';
 
 export const App = () => {
   return (
     <div>
       <Navbar/>
-      <Homepage/>
+      {/* <Homepage/> */}
+      <SearchBooksPage/>
       <Footer/>
     </div>
   );
