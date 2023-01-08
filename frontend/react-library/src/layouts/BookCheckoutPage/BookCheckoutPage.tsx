605a763c6114f8b34bb6920615c9f2068791c443
Matthew Guizar <matthewrguizar@gmail.com>
2023-01-08 10:40:10 -0800

    added checkout and review box component

--- frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { StarsReview } from "../Utils/StarsReview";
+import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
 
 export const BookCheckoutPage = () => {
 
@@ -74,8 +76,10 @@ export const BookCheckoutPage = () => {
                             <h2>{book?.title}</h2>
                             <h5 className='text-primary'>{book?.author}</h5>
                             <p className='lead'>{book?.description}</p>
+                            <StarsReview rating={4.5} size={32}/>
                         </div>
                     </div>
+                    <CheckoutAndReviewBox book={book} mobile={false}/>
                 </div>
                 <hr />
             </div>
@@ -93,8 +97,10 @@ export const BookCheckoutPage = () => {
                         <h2>{book?.title}</h2>
                         <h5 className='text-primary'>{book?.author}</h5>
                         <p className='lead'>{book?.description}</p>
+                        <StarsReview rating={4.5} size={32}/>
                     </div>
                 </div>
+                <CheckoutAndReviewBox book={book} mobile={true}/>
                 <hr />
             </div>
         </div>
