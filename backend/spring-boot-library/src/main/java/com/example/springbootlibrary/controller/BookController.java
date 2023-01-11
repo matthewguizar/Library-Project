6b0bb0701edad8612bc23413bcb48a36ee2ca20b
Matthew Guizar <matthewrguizar@gmail.com>
2023-01-10 18:16:23 -0800

    added controller for checkout

---
+++ backend/spring-boot-library/src/main/java/com/example/springbootlibrary/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.example.springbootlibrary.controller;
+
+
+import com.example.springbootlibrary.entity.Book;
+import com.example.springbootlibrary.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook (@RequestParam Long bookId) throws Exception {
+        String userEmail = "test@email.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+}
