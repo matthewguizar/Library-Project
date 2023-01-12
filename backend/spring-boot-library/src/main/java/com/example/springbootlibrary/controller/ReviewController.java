0247b472ca0e63b138f65036f9a774fd060f5313
Matthew Guizar <matthewrguizar@gmail.com>
2023-01-12 12:41:08 -0800

    review controller

---
+++ backend/spring-boot-library/src/main/java/com/example/springbootlibrary/controller/ReviewController.java
@@ -0,0 +1,28 @@
+package com.example.springbootlibrary.controller;
+
+import com.example.springbootlibrary.requestmodels.ReviewRequest;
+import com.example.springbootlibrary.service.ReviewService;
+import com.example.springbootlibrary.utils.ExtractJWT;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping("/secure")
+    public void postReview(@RequestHeader(value="Authorization") String token,
+                           @RequestBody ReviewRequest reviewRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if (userEmail == null) {
+            throw new Exception("User email is missing ");
+        }
+        reviewService.postReview(userEmail, reviewRequest);
+    }
+}
